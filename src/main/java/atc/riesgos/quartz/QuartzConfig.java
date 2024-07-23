7e9696bb7b9a22e035babc5657ee1647a6d00526
Abel Copa <is.elmer.inf@gmail.com>
2024-07-23 12:04:08 -0400

    Correcion bug zona horaria

--- src/main/java/atc/riesgos/quartz/QuartzConfig.java
+++ src/main/java/atc/riesgos/quartz/QuartzConfig.java
@@ -33,6 +33,6 @@ public class QuartzConfig {
                 .withSchedule(CronScheduleBuilder.cronSchedule(cronSchedule)
                 .inTimeZone(TimeZone.getTimeZone("America/La_Paz")))
                 .build();
-    }
+    } 
 }
 
